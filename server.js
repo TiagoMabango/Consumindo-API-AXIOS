14741fecf66307fd15e1073bc1a2391f40af5de7
PedroMakengo <61626420+PedroMakengo@users.noreply.github.com>
2022-01-28 08:34:16 -0300

    Adicionando algumas melhorias

--- server.js
+++ server.js
@@ -1,18 +1,16 @@
-const cors = require('cors')
-const express = require('express')
-const app = express()
-const axios = require('axios')
+const cors = require("cors");
+const express = require("express");
+const app = express();
+const axios = require("axios");
 
-app.use(cors())
+app.use(cors());
 
-app.get('/',async(req,res)=>{
-
-    try {
-        
-        const { data } = await axios('https://jsonplaceholder.typicode.com/users')
-        return res.json(data)
-    } catch (error) {
-      console.log(error)  
-    }
-})
-app.listen('4567')
+app.get("/", async (req, res) => {
+  try {
+    const { data } = await axios("https://jsonplaceholder.typicode.com/users");
+    return res.json(data);
+  } catch (error) {
+    console.log(error);
+  }
+});
+app.listen("4567");
